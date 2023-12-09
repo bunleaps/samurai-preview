78d137e96eef0586ef0b7a6e1f5baff3e9b51d18
bunleaps <bunleap.sorn@gmail.com>
2023-12-09 19:22:09 +0700

    company code changes

--- src/app/company_code/page.js
+++ src/app/company_code/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./hero";
 import Details from "./details";
+import TrustedPartners from "@/components/trusted_partners";
 
 function CompanyCode() {
   return (
     <div>
       <Hero />
       <Details />
+      <TrustedPartners />
     </div>
   );
 }
